dd09b02700fffb9697f27daf3c8e646a4adcff2e
Sebastian Mendel <sebastian.mendel@netresearch.de>
2018-02-27 17:47:02 +0100

    update docker-lib.sh with content from https://github.com/concourse/docker-image-resource/blob/master/assets/common.sh

--- docker-lib.sh
+++ docker-lib.sh
@@ -1,4 +1,5 @@
-# Ref: https://github.com/concourse/docker-image-resource/blob/master/assets/common.sh
+LOG_FILE=${LOG_FILE:-/tmp/docker.log}
+SKIP_PRIVILEGED=${SKIP_PRIVILEGED:-false}
 
 sanitize_cgroups() {
   mkdir -p /sys/fs/cgroup
@@ -44,28 +45,30 @@ start_docker() {
   mkdir -p /var/log
   mkdir -p /var/run
 
-  sanitize_cgroups
+  if [ "$SKIP_PRIVILEGED" = "false" ]; then
+    sanitize_cgroups
 
-  # check for /proc/sys being mounted readonly, as systemd does
-  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
-    mount -o remount,rw /proc/sys
+    # check for /proc/sys being mounted readonly, as systemd does
+    if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+      mount -o remount,rw /proc/sys
+    fi
   fi
 
   local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
   local server_args="--mtu ${mtu}"
   local registry=""
 
-  for registry in $1; do
+  server_args="${server_args} --max-concurrent-downloads=$1 --max-concurrent-uploads=$2"
+
+  for registry in $3; do
     server_args="${server_args} --insecure-registry ${registry}"
   done
 
-  if [ -n "$2" ]; then
-    server_args="${server_args} --registry-mirror=$2"
+  if [ -n "$4" ]; then
+    server_args="${server_args} --registry-mirror $4"
   fi
 
-  server_args="${server_args} --experimental"
-
-  dockerd --data-root /scratch/docker ${server_args} >/tmp/docker.log 2>&1 &
+  dockerd --data-root /scratch/docker ${server_args} >$LOG_FILE 2>&1 &
   echo $! > /tmp/docker.pid
 
   trap stop_docker EXIT
@@ -84,5 +87,106 @@ stop_docker() {
     return 0
   fi
 
-  kill $pid
+  kill -TERM $pid
+}
+
+log_in() {
+  local username="$1"
+  local password="$2"
+  local registry="$3"
+
+  if [ -n "${username}" ] && [ -n "${password}" ]; then
+    docker login -u "${username}" -p "${password}" ${registry}
+  else
+    mkdir -p ~/.docker
+    echo '{"credsStore":"ecr-login"}' >> ~/.docker/config.json
+  fi
+}
+
+private_registry() {
+  local repository="${1}"
+
+  if echo "${repository}" | fgrep -q '/' ; then
+    local registry="$(extract_registry "${repository}")"
+    if echo "${registry}" | fgrep -q '.' ; then
+      return 0
+    fi
+  fi
+
+  return 1
+}
+
+extract_registry() {
+  local repository="${1}"
+
+  echo "${repository}" | cut -d/ -f1
+}
+
+extract_repository() {
+  local long_repository="${1}"
+
+  echo "${long_repository}" | cut -d/ -f2-
+}
+
+image_from_tag() {
+  docker images --no-trunc "$1" | awk "{if (\$2 == \"$2\") print \$3}"
+}
+
+image_from_digest() {
+  docker images --no-trunc --digests "$1" | awk "{if (\$3 == \"$2\") print \$4}"
+}
+
+certs_to_file() {
+  local raw_ca_certs="${1}"
+  local cert_count="$(echo $raw_ca_certs | jq -r '. | length')"
+
+  for i in $(seq 0 $(expr "$cert_count" - 1));
+  do
+    local cert_dir="/etc/docker/certs.d/$(echo $raw_ca_certs | jq -r .[$i].domain)"
+    mkdir -p "$cert_dir"
+    echo $raw_ca_certs | jq -r .[$i].cert >> "${cert_dir}/ca.crt"
+  done
+}
+
+set_client_certs() {
+  local raw_client_certs="${1}"
+  local cert_count="$(echo $raw_client_certs | jq -r '. | length')"
+
+  for i in $(seq 0 $(expr "$cert_count" - 1));
+  do
+    local cert_dir="/etc/docker/certs.d/$(echo $raw_client_certs | jq -r .[$i].domain)"
+    [ -d "$cert_dir" ] || mkdir -p "$cert_dir"
+    echo $raw_client_certs | jq -r .[$i].cert >> "${cert_dir}/client.cert"
+    echo $raw_client_certs | jq -r .[$i].key >> "${cert_dir}/client.key"
+  done
+}
+
+docker_pull() {
+  GREEN='\033[0;32m'
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+
+  pull_attempt=1
+  max_attempts=3
+  while [ "$pull_attempt" -le "$max_attempts" ]; do
+    printf "Pulling ${GREEN}%s${NC}" "$1"
+
+    if [ "$pull_attempt" != "1" ]; then
+      printf " (attempt %s of %s)" "$pull_attempt" "$max_attempts"
+    fi
+
+    printf "...\n"
+
+    if docker pull "$1"; then
+      printf "\nSuccessfully pulled ${GREEN}%s${NC}.\n\n" "$1"
+      return
+    fi
+
+    echo
+
+    pull_attempt=$(expr "$pull_attempt" + 1)
+  done
+
+  printf "\n${RED}Failed to pull image %s.${NC}" "$1"
+  exit 1
 }
